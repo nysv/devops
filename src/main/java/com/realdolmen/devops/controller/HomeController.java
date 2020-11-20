260b35045c12bbe23a79f2532e53f06c295c9d8d
Vincent Nys <vincent.nys@realdolmen.com>
2020-11-20 11:08:44 +0100

    adding project

---
+++ src/main/java/com/realdolmen/devops/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.realdolmen.devops.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+
+    @RequestMapping("/")
+    public String viewHome(Model model) {
+        return "home";
+    }
+
+}
